f4ad254dfbc7835bda599372e18fc5ec6cbb53ed
Bice93 <bianca.cerulo@hotmail.it>
2022-06-28 09:15:45 +0200

    Add block to click

--- js/main.js
+++ js/main.js
@@ -12,46 +12,49 @@
 //const containerGrid = document.getElementById(`container_square`); //recupero il parent dei quadratini
 const buttonPlay = document.getElementById(`btn_play`); //recupero il bottone play
 const boxPlayed = document.getElementById(`box_play`);
+let lock = false; //per bloccare il click dopo la bomba iniziallizo lock a false
+
 
 
 buttonPlay.addEventListener(`click`, function () { //al click del bottone play visualizzo la griglia di gioco
+    let lock = false;
     const listaNumeri = []; // al click genero numeri da 1 a 16 per le bombe
     for (let i = 0; i < 16; i++) {
-        const numeroRandomico = generoNumeroRandomico(listaNumeri, 1, 17);
+        const numeroRandomico = generoNumeroRandomico(listaNumeri, 1, 101);
         listaNumeri.push(numeroRandomico);
+        console.log(numeroRandomico)
     }
-
+    console.log(listaNumeri)
     const containerGrid = document.createElement(`div`);   //creo la griglia dove vado ad inserire tutti i quadrati
     containerGrid.classList.add(`container_square`);   //aggiungo alla griglia la classe 
     boxPlayed.innerHTML = "";
-    const listNumberContainer = [];
     for (let i = 1; i < 101; i++) {
         const square = createSquare();
-        const numberRandomContainer = generoNumeroRandomico(listNumberContainer, 1, 101);
-        listNumberContainer.push(numberRandomContainer)
-        //square.innerText = numberRandomContainer;   // aggiungo il numero al quadratino
+        //square.innerText = i;   // aggiungo il numero al quadratino
         containerGrid.append(square);   // aggiungo al parent il quadratino per 100 volte con il ciclo
 
-        square.addEventListener(`click`, function () {     // al click del quadratino 
-            if (listaNumeri.includes(numberRandomContainer)){    //se la lista contiene un numero randomico uguale al numero randomico del quadratino(figlio del contenitore)
-                square.classList.add(`bomb`);
-                square.innerText = numberRandomContainer;
-                console.log(`Hai preso la bomba`)   //ha preso la bomba
+        square.addEventListener(`click`, function () { // al click del quadratino 
+
+            if (lock === true) { 
+
             } else {
-                square.classList.add(`active`);    // altriementi continua a cliccare
-                console.log(`Hai cliccato la casella: ${i} con numero randomico ${numberRandomContainer}`);
+                square.classList.add(`active`);    // aggiungo la classe active
+                console.log(`Hai cliccato la casella: ${i}`);
+
+
+                if (listaNumeri.includes(i)) {    //se la lista contiene un numero randomico uguale al numero randomico del quadratino(figlio del contenitore)
+                    square.classList.add(`bomb`);
+                    console.log(`Hai preso la bomba`)   //ha preso la bomba
+                    alert(`Hai perso!`);
+                    lock = true;
+                }
             }
-            //console.log(listaNumeri.includes(numberRandomContainer))
-           // console.log(listaNumeri.includes)
-            
+
         });
-       
     }
     boxPlayed.append(containerGrid); //aggiungo al parent TUTTA la griglia
-
+    return boxPlayed.append(containerGrid);
 });
-
-
 
 
 // ........................FUNZIONI.......................
@@ -60,7 +63,6 @@ function createSquare() { //creo il quadratino e gli assegno le classi che vogli
     newSquare.classList.add(`square`);
     return newSquare;  //restituisco newSquare così lo posso assegnare a square
 }
-
 
 function generoNumeroRandomico(lista, min, max) {
     let nuovoNumero;
@@ -71,6 +73,7 @@ function generoNumeroRandomico(lista, min, max) {
             numeroPresente = true;
         }
     };
-
     return nuovoNumero;
 };
+
+
